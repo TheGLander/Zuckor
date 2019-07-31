1591713abd1a1a615b459dbc603a59af8bb0d62c
Lubomir <lubomir.yudenko@gmail.com>
2019-07-31 12:25:55 +0300

    Sprite.collisionWith() added

--- engine.js
+++ engine.js
@@ -94,6 +94,7 @@
                 enumerable: true,
                 configurable: true,
                 value: function getCollisions() {
+                    console.warn("Sprite.getCollisions() is deprecated and may be removed in the next version. Use Sprite.collisionWith() instead.")
                     var collisions = []
                     for (var i in sprites) {
                         if (sprites[i].id !== this.id) {
@@ -107,6 +108,25 @@
                         }
                     }
                     return collisions
+                }
+            });
+            // 
+            Object.defineProperty(this, "collisionWith", {
+                writable: false,
+                enumerable: true,
+                configurable: true,
+                value: function collisionWith(sprite) {
+                    if (Math.abs((this.x + spriteImg[this.image].image.width / 2) - (sprite.x + spriteImg[sprite.image].image.width / 2)) <= spriteImg[this.image].image.width && Math.abs((this.y + spriteImg[this.image].image.height / 2) - (sprite.y + spriteImg[sprite.image].image.height / 2)) <= spriteImg[this.image].image.height) {
+                            for (var x in spriteImg[sprite.image].pixels) {
+                                for (var z in spriteImg[this.image].pixels) {
+                                    if (spriteImg[this.image].pixels[z].x + this.x == spriteImg[sprite.image].pixels[x].x + sprite.x && spriteImg[this.image].pixels[z].y + this.y == spriteImg[sprite.image].pixels[x].y + sprite.y) {
+                                        return true
+                                    }
+                                }
+                            }
+                        }
+                    
+                    return false
                 }
             });
             //Physics
