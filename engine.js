34ff4259345fb479418a6f0a8969f03a379e2c05
Lubomir <lubomir.yudenko@gmail.com>
2019-08-18 22:34:35 +0300

    Just a recall to stuff

--- engine.js
+++ engine.js
@@ -13,13 +13,13 @@
             width: width = 500,
             color: color = undefined,
             canvas: canvas = document.getElementById("game"),
-            framerate: framerate = 1000
+            framerate: framerate = 60
         } = {
             height: 500,
             width: 500,
             color: undefined,
             canvas: document.getElementById("game"),
-            framerate: 1000
+            framerate: 60
         }) {
             frameRate = framerate
             canvas.height = height
@@ -43,24 +43,24 @@
                     return ((x < y) ? -1 : ((x > y) ? 1 : 0));
                 }).reverse();
                 for (var i in orderedSprites) {
-                    if (!(Object.keys(spriteImg).includes(orderedSprites[i].image.imageName))) {
-                        spriteImg[orderedSprites[i].image.imageName] = {}
-                        spriteImg[orderedSprites[i].image.imageName].image = new Image()
-                        spriteImg[orderedSprites[i].image.imageName].image.src = orderedSprites[i].image.imageName;
-                        spriteImg[orderedSprites[i].image.imageName].image["data-i"] = i
-                        spriteImg[orderedSprites[i].image.imageName].image["data-image"] = orderedSprites[i].image.imageName
-                        spriteImg[orderedSprites[i].image.imageName].image.onload = function (ev) {
-                            drawRotatedImage(context, spriteImg[ev.currentTarget["data-image"]].image, orderedSprites[ev.currentTarget["data-i"]].x, orderedSprites[ev.currentTarget["data-i"]].y, orderedSprites[ev.currentTarget["data-i"]].degree, orderedSprites[ev.currentTarget["data-i"]].image.height, orderedSprites[ev.currentTarget["data-i"]].image.width)
+                    if (!(Object.keys(spriteImg).includes(orderedSprites[i].image))) {
+                        spriteImg[orderedSprites[i].image] = {}
+                        spriteImg[orderedSprites[i].image].image = new Image()
+                        spriteImg[orderedSprites[i].image].image.src = orderedSprites[i].image;
+                        spriteImg[orderedSprites[i].image].image["data-i"] = i
+                        spriteImg[orderedSprites[i].image].image["data-image"] = orderedSprites[i].image
+                        spriteImg[orderedSprites[i].image].image.onload = function (ev) {
+                            drawRotatedImage(context, spriteImg[ev.currentTarget["data-image"]].image, orderedSprites[ev.currentTarget["data-i"]].x, orderedSprites[ev.currentTarget["data-i"]].y, orderedSprites[ev.currentTarget["data-i"]].y)
                             spriteImg[ev.currentTarget["data-image"]].pixels = getNonTransparentPixels(ev.currentTarget)
                         }
                     } else {
-                        drawRotatedImage(context, spriteImg[orderedSprites[i].image.imageName].image, orderedSprites[i].x, orderedSprites[i].y, orderedSprites[i].degree, orderedSprites[i].image.height, orderedSprites[i].image.width)
+                        drawRotatedImage(context, spriteImg[orderedSprites[i].image].image, orderedSprites[i].x, orderedSprites[i].y, orderedSprites[i].degree)
                     }
 
                 }
             }, 1000 / frameRate)
         }
-        _myLibraryObject.Sprite = function ([x, y, layer, degree] = [0, 0, 0], [image, width, height] = [], nickname) {
+        __myLibraryObject.Sprite = function ([x, y, layer, degree] = [0, 0, 0], image, nickname) {
             this.x = x || 0
             this.y = y || 0
             this.degree = degree || 0
@@ -82,10 +82,7 @@
                     return Object.keys(sprites).length
                 })()
             });
-            this.image = {}
-            this.image.imageName = image || ""
-            this.image.height = height || null
-            this.image.width = width || null
+            this.image = image
             this.physics = {}
             //Delete
             Object.defineProperty(this, "delete", {
@@ -103,7 +100,7 @@
                 enumerable: true,
                 configurable: true,
                 value: function collisionWith(sprite) {
-                    return (Math.abs((this.x + sprite.image.width / 2) - (sprite.x + sprite.image.width / 2)) <= sprite.image.width / 2 + sprite.image.width / 2 && Math.abs((this.y + sprite.image.height / 2) - (sprite.y + sprite.image.height / 2)) <= sprite.image.height / 2 + sprite.image.height / 2)
+                    return (Math.abs((this.x + spriteImg[this.image].image.width / 2) - (sprite.x + spriteImg[sprite.image].image.width / 2)) <= spriteImg[this.image].image.width / 2 + spriteImg[sprite.image].image.width / 2 && Math.abs((this.y + spriteImg[this.image].image.height / 2) - (sprite.y + spriteImg[sprite.image].image.height / 2)) <= spriteImg[this.image].image.height / 2 + spriteImg[sprite.image].image.height / 2)
                 }
             });
             Object.defineProperty(this, "collisionWithSolid", {
@@ -161,28 +158,28 @@
                     this.physics.solid = solid //If the object is solid
                     this.physics.calcId = setInterval(() => {
                         //Gravity movement calculation(No loss)
-                        this.physics.gravityVelocity += this.physics.gravityAcceleration / 1000
+                        this.physics.gravityVelocity += this.physics.gravityAcceleration / frameRate
                         if (!this.collisionWithSolid()) {
-                            this.x += this.physics.gravityVelocity / 1000 * Math.cos(rad(this.physics.gravityDegree));
+                            this.x += this.physics.gravityVelocity / frameRate * Math.cos(rad(this.physics.gravityDegree));
                         } else {
                             this.physics.gravityVelocity = 0
                         }
                         if (!this.collisionWithSolid()) {
-                            this.y += this.physics.gravityVelocity / 1000 * Math.sin(rad(this.physics.gravityDegree));
+                            this.y += this.physics.gravityVelocity / frameRate * Math.sin(rad(this.physics.gravityDegree));
                         } else {
                             this.physics.gravityVelocity = 0
                         }
                         //Normal movement calculation(With loss)
-                        this.physics.velocity += this.physics.acceleration / 1000
-                        this.physics.velocity -= (this.physics.velocity - this.physics.velocityLoss / 1000 < 0 ? this.physics.velocity : this.physics.velocityLoss / 1000)
-                        this.physics.acceleration -= (this.physics.acceleration - this.physics.accelerationLoss / 1000 < 0 ? this.physics.acceleration : this.physics.accelerationLoss / 1000)
+                        this.physics.velocity += this.physics.acceleration / frameRate
+                        this.physics.velocity -= (this.physics.velocity - this.physics.velocityLoss / frameRate < 0 ? this.physics.velocity : this.physics.velocityLoss / frameRate)
+                        this.physics.acceleration -= (this.physics.acceleration - this.physics.accelerationLoss / frameRate < 0 ? this.physics.acceleration : this.physics.accelerationLoss / frameRate)
                         if (!this.collisionWithSolid()) {
-                            this.x += this.physics.velocity / 1000 * Math.cos(rad(this.physics.degree));
+                            this.x += this.physics.velocity / frameRate * Math.cos(rad(this.physics.degree));
                         } else {
                             this.physics.velocity = 0
                         }
                         if (!this.collisionWithSolid()) {
-                            this.y += this.physics.velocity / 1000 * Math.sin(rad(this.physics.degree));
+                            this.y += this.physics.velocity / frameRate * Math.sin(rad(this.physics.degree));
                         } else {
                             this.physics.velocity = 0
                         }
--- functions.js
+++ functions.js
@@ -36,7 +36,7 @@ function rad(degrees) {
     return degrees * (Math.PI / 180);
 }
 
-function drawRotatedImage(context, image, x, y, angle, height, width) {
+function drawRotatedImage(context, image, x, y, angle) {
 
     x += image.width / 2
     y += image.height / 2
@@ -53,7 +53,7 @@ function drawRotatedImage(context, image, x, y, angle, height, width) {
 
     // draw it up and to the left by half the width
     // and height of the image 
-    context.drawImage(image, -(image.width / 2), -(image.height / 2), height === null ? image.height : height, width === null ? image.width : width);
+    context.drawImage(image, -(image.width / 2), -(image.height / 2));
 
     // and restore the co-ords to how they were when we began
     context.restore();
